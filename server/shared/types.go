90f6f0f10f14971c941afd30484f35c06bf55bf0
Dale-Kurt Murray <hello@dalekurtmurray.com>
2024-02-14 12:51:38 -0500

    Moved the JobDetails struct to a shared

---
+++ server/shared/types.go
@@ -0,0 +1,12 @@
+// server/shared/types.go
+package shared
+
+// JobDetails struct to pass job-related data to the workflow and activities
+type JobDetails struct {
+	ID          string
+	Name        string
+	Description string
+	Filename    string
+	GitRepo     string
+	GitBranch   string
+}
