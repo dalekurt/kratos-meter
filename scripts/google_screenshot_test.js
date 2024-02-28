50177ff8ff3ca9e9b4e7b53cff46bba2a2a0831e
Dale-Kurt Murray <hello@dalekurtmurray.com>
2024-02-28 17:21:34 -0500

    Fixed the screenshot test script

--- scripts/google_screenshot_test.js
+++ scripts/google_screenshot_test.js
@@ -1,22 +1,42 @@
-// google_screenshot_test.js
 import { browser } from 'k6/experimental/browser';
 
 export const options = {
     scenarios: {
-        browser_test: {
-            executor: 'browser',
+        ui: {
+            executor: 'shared-iterations',
             vus: 1,
             iterations: 1,
-            browser: {
-                type: 'chromium', // or 'firefox', depending on your needs and setup
+            options: {
+                browser: {
+                    type: 'chromium',
+                },
             },
         },
     },
+    thresholds: {
+        checks: ['rate==1.0'],
+    },
 };
 
-export default function () {
+export default async function () {
     const page = browser.newPage();
-    page.goto('https://www.google.com/');
-    page.screenshot({ path: '/tmp/screenshot_google.png', fullPage: true });
-    page.close();
+    // Generate a timestamp string in the format "YYYYMMDD_HHMMSS"
+    const timestamp = new Date().toISOString().replace(/[-:.TZ]/g, '').slice(0, -4);
+    // Prefix the filename with "screenshot" and append the timestamp
+    const screenshotFilename = `screenshot_${timestamp}.png`;
+
+    try {
+        await page.goto('https://google.com/');
+        try {
+            // Use the dynamically generated filename for the screenshot
+            await page.screenshot({ path: screenshotFilename });
+            console.log(`Screenshot saved as ${screenshotFilename}`);
+        } catch (screenshotError) {
+            console.error(`Screenshot failed: ${screenshotError}`);
+        }
+    } catch (navigationError) {
+        console.error(`Navigation failed: ${navigationError}`);
+    } finally {
+        await page.close();
+    }
 }
