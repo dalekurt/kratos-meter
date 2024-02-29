b96057b6fc188a17a87eb02e9701b02e55223c86
Dale-Kurt Murray <hello@dalekurtmurray.com>
2024-02-29 12:47:04 -0500

    Updated script to include the jobID as an env var

--- scripts/google_screenshot_test.js
+++ scripts/google_screenshot_test.js
@@ -20,22 +20,16 @@ export const options = {
 
 export default async function () {
     const page = browser.newPage();
-    // Generate a timestamp string in the format "YYYYMMDD_HHMMSS"
+    const jobID = __ENV.JOB_ID;
     const timestamp = new Date().toISOString().replace(/[-:.TZ]/g, '').slice(0, -4);
-    // Prefix the filename with "screenshot" and append the timestamp
-    const screenshotFilename = `/tmp/screenshot_${timestamp}.png`;
+    const screenshotFilename = `/tmp/screenshot_${jobID}_${timestamp}.png`;
 
     try {
         await page.goto('https://google.com/');
-        try {
-            // Use the dynamically generated filename for the screenshot
-            await page.screenshot({ path: screenshotFilename });
-            console.log(`Screenshot saved as ${screenshotFilename}`);
-        } catch (screenshotError) {
-            console.error(`Screenshot failed: ${screenshotError}`);
-        }
-    } catch (navigationError) {
-        console.error(`Navigation failed: ${navigationError}`);
+        await page.screenshot({ path: screenshotFilename });
+        console.log(`Screenshot saved as ${screenshotFilename}`);
+    } catch (error) {
+        console.error(`Error: ${error}`);
     } finally {
         await page.close();
     }
