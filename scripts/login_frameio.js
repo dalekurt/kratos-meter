c70344e086078c34fffdca524b191d8172a82733
Dale-Kurt Murray <hello@dalekurtmurray.com>
2024-03-08 17:42:29 -0500

    Increase the page timeout

--- scripts/login_frameio.js
+++ scripts/login_frameio.js
@@ -6,6 +6,7 @@ export const options = {
             executor: 'shared-iterations',
             vus: 1,
             iterations: 1,
+            vusMax: 1, // Ensuring only 1 virtual user
             options: {
                 browser: {
                     type: 'chromium',
@@ -17,26 +18,23 @@ export const options = {
 
 export default async function () {
     const page = browser.newPage();
-    const email = __ENV.EMAIL; // Retrieved from environment variables
-    const password = __ENV.PASSWORD; // Retrieved from environment variables
-    const jobID = __ENV.JOB_ID; // Ensure JOB_ID is passed as an environment variable
+    const email = __ENV.EMAIL; 
+    const password = __ENV.PASSWORD;
+    const jobID = __ENV.JOB_ID; 
     const timestamp = new Date().toISOString().replace(/[-:.TZ]/g, '').slice(0, -4);
 
-    await page.goto('https://app.frame.io/login');
-    await page.waitForSelector('input[name="email"]');
-    
-    // Take a screenshot of the login page
+    // Increased timeout for page navigation
+    await page.goto('https://app.frame.io/login', { timeout: 60000 });
+    await page.waitForSelector('input[name="email"]', { timeout: 60000 });
+
     await page.screenshot({ path: `/tmp/screenshot_${jobID}_loginPage_${timestamp}.png` });
 
-    // Fill in the email and password fields and submit the form
     await page.fill('input[name="email"]', email);
     await page.fill('input[name="password"]', password);
-    await page.click('button[type="submit"]'); // Adjust the selector to match the login button
+    await page.click('button[type="submit"]');
+    
+    await page.waitForNavigation({ waitUntil: 'networkidle', timeout: 60000 });
 
-    // Wait for navigation to the dashboard or subsequent page
-    await page.waitForNavigation({ waitUntil: 'networkidle' });
-
-    // Take a screenshot after successful login
     await page.screenshot({ path: `/tmp/screenshot_${jobID}_afterLogin_${timestamp}.png` });
 
     console.log(`Screenshots saved with Job ID: ${jobID} and Timestamp: ${timestamp}`);
