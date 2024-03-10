b20931ac312544405f2bdb2398bbef939ced69d6
Dale-Kurt Murray <hello@dalekurtmurray.com>
2024-03-10 19:10:13 -0400

    Updated k6 script

--- scripts/login_frameio.js
+++ scripts/login_frameio.js
@@ -6,7 +6,6 @@ export const options = {
             executor: 'shared-iterations',
             vus: 1,
             iterations: 1,
-            gracefulStop: '30s',
             options: {
                 browser: {
                     type: 'chromium',
@@ -18,19 +17,16 @@ export const options = {
 
 export default async function () {
     const page = browser.newPage();
-    const jobID = __ENV.JOB_ID; // Ensure JOB_ID is passed as an environment variable
+    const jobID = __ENV.JOB_ID || 'default'; // Fallback to 'default' if JOB_ID is not set
     const timestamp = new Date().toISOString().replace(/[-:.TZ]/g, '').slice(0, -4);
 
-    try {
-        await page.goto('https://www.frame.io', { waitUntil: 'networkidle', timeout: 120000 });
-        // Additional waitForLoadState can be redundant but used here for demonstration
-        await page.waitForLoadState('networkidle');
-        await page.screenshot({ path: `/tmp/screenshot_${jobID}_frameio_${timestamp}.png` });
+    // Navigate to the website
+    await page.goto('https://www.frame.io', { waitUntil: 'load' });
 
-        console.log(`Screenshot saved with Job ID: ${jobID} and Timestamp: ${timestamp}`);
-    } catch (error) {
-        console.log(`An error occurred: ${error.message}`);
-    } finally {
-        await page.close();
-    }
+    // Take a screenshot after the page is completely loaded
+    await page.screenshot({ path: `/tmp/screenshot_${jobID}_${timestamp}.png` });
+
+    console.log(`Screenshot saved with Job ID: ${jobID} and Timestamp: ${timestamp}`);
+
+    await page.close();
 }
