3577d430475707a83b4fcd6d3df353eb59c78935
Dale-Kurt Murray <hello@dalekurtmurray.com>
2024-03-10 18:56:32 -0400

    Updated k6 script

--- scripts/login_frameio.js
+++ scripts/login_frameio.js
@@ -4,9 +4,8 @@ export const options = {
     scenarios: {
         ui: {
             executor: 'shared-iterations',
-            vus: 1,
+            vus: 1, // Define the number of VUs here
             iterations: 1,
-            vusMax: 1, // Define the maximum number of virtual users
             options: {
                 browser: {
                     type: 'chromium',
@@ -21,20 +20,12 @@ export default async function () {
     const jobID = __ENV.JOB_ID; // Ensure JOB_ID is passed as an environment variable
     const timestamp = new Date().toISOString().replace(/[-:.TZ]/g, '').slice(0, -4);
 
-    // Optionally block images and CSS for faster loading if not needed for the screenshot
-    // Uncomment below if you want to block certain types of resources
-    // await page.route('**/*.{png,jpg,jpeg,gif,css}', route => route.abort());
+    await page.goto('https://www.frame.io', { waitUntil: 'networkidle' });
+    // Ensure the page has loaded before taking a screenshot
+    await page.waitForLoadState('networkidle');
+    await page.screenshot({ path: `/tmp/screenshot_${jobID}_frameio_${timestamp}.png` });
 
-    try {
-        await page.goto('https://www.frame.io', { waitUntil: 'networkidle', timeout: 60000 });
-
-        // Take a screenshot of the page once it's fully loaded
-        await page.screenshot({ path: `/tmp/screenshot_${jobID}_frameioComplete_${timestamp}.png` });
-
-        console.log(`Screenshot of complete page load saved with Job ID: ${jobID} and Timestamp: ${timestamp}`);
-    } catch (e) {
-        console.error(`Test execution error: ${e}`);
-    } finally {
-        await page.close();
-    }
+    console.log(`Screenshot saved with Job ID: ${jobID} and Timestamp: ${timestamp}`);
+    
+    await page.close();
 }
